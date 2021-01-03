de2dca4b86b4fc6debe6216e762b30f0ba3736d7
root <root@LH-007.localdomain>
2021-01-03 23:05:02 +0000

    creation du repository, avec les dossier des différentes manière d'ecrire un APi REST

---
+++ envfastapi/Lib/site-packages/fastapi_restful/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.4.3"
+
+from .cbv_base import Api, Resource, set_responses, take_init_parameters
+
+__all__ = [
+    "Api",
+    "Resource",
+    "set_responses",
+    "take_init_parameters",
+]
